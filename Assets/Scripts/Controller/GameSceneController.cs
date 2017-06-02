e1ea84e04756c0813b3b7f85c833e835eb89a117
mohaor <664858757@qq.com>
2017-06-03 00:31:32 +0800

    目前状况：改为2d；插入背景和部分素材；点击空地能弹出选择框；点击选择框能够建起炮塔

---
+++ Assets/Scripts/Controller/GameSceneController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.TowerDefence2d {
+
+    public class GameSceneController : System.Object, UserClickOp {
+        private static GameSceneController instance;
+        private GameStatus myGameStatus;
+        private GameModels myGameModels;
+
+        public static GameSceneController getInstance() {
+            if (instance == null)
+                instance = new GameSceneController();
+            return instance;
+        }
+
+        internal void setGameStatus(GameStatus _myGameStatus) {
+            if (myGameStatus == null) {
+                myGameStatus = _myGameStatus;
+            }
+        }
+
+        internal void setGameModels(GameModels _myGameModels) {
+            if (myGameModels == null) {
+                myGameModels = _myGameModels;
+            }
+        }
+
+        public void setBuildBtnGroupPos(int stubIndex) {
+            myGameModels.setBuildBtnGroupPos(stubIndex);
+        }
+
+        public int getChosenStubNum() {
+            return myGameStatus.getChosenStubNum();
+        }
+
+        public void setClickedStubIndex(int index) {
+            myGameStatus.setClickedStubIndex(index);
+        }
+
+        public void buildArrowTower() {
+            myGameModels.buildArrowTower();
+        }
+
+        public void buildSoldierTower() {
+            myGameModels.buildSoldierTower();
+        }
+
+        public void buildWizardTower() {
+            myGameModels.buildWizardTower();
+        }
+
+        public void buildCannonTower() {
+            myGameModels.buildCannonTower();
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/View/UserInterface.cs
@@ -0,0 +1,39 @@
+﻿using Com.TowerDefence2d;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour {
+
+    UserClickOp userClickOp;
+
+    void Start () {
+        userClickOp = GameSceneController.getInstance() as UserClickOp;
+
+    }
+	
+	void Update () {
+		
+	}
+
+    public void onGUIBtnClick() {
+        //Debug.Log("onGUIBtnClick! " + transform.position + " " + this.name);
+
+        if (this.tag.Equals("Stub")) {
+            string stubName = this.name;
+            int index = stubName[stubName.Length - 1] - '0' - 1;
+            userClickOp.setClickedStubIndex(index);
+        }
+        else if (this.tag.Equals("BuildBtn")) {
+            string btnName = this.name;
+            if (btnName.Contains("Arrow"))
+                userClickOp.buildArrowTower();
+            else if (btnName.Contains("Soldier"))
+                userClickOp.buildSoldierTower();
+            else if (btnName.Contains("Wizard"))
+                userClickOp.buildWizardTower();
+            else if (btnName.Contains("Cannon"))
+                userClickOp.buildCannonTower();
+        }
+    }
+}
