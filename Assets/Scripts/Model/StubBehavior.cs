47a6f96ab334f00399a73b6531c0b3cce57da54d
mohaor <664858757@qq.com>
2017-06-03 23:51:12 +0800

    实现抛物线发射炮弹；添加炮弹工厂

--- Assets/Scripts/Model/StubBehavior.cs
+++ Assets/Scripts/Model/StubBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Com.TowerDefence2d;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,12 +14,28 @@ public class StubBehavior : MonoBehaviour {
 
     void Start () {
         myImage = this.gameObject.GetComponent<Image>();
-
+        
     }
 	
 	void Update () {
-		
+        detectKeyboardInput();
 	}
+
+    void detectKeyboardInput() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (stubType == StubType.CannonTower) {
+                Debug.Log(this.gameObject.name + " Fire");
+                Vector2 currPos = new Vector2(this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y  + 0.5f);
+
+                int dirY = Random.Range(-1, 2);
+                Debug.Log("dirY " + dirY);
+                Vector2 tarPos = new Vector2(this.gameObject.transform.position.x + 1.5f, 
+                    this.gameObject.transform.position.y + 1.0f * (float)dirY);
+                GameObject cannonBall = CannonBallFactory.getInstance().generateCannonBall(currPos, tarPos);
+            }
+        }
+    }
 
     public void buildArrowTower() {
         stubType = StubType.ArrowTower;
